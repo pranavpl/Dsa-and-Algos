449b38bfe112f395ce7bb167850dc3601c175c58
Pranav.PL <pranavpl.tvm@gmail.com>
2025-01-06 22:04:34 +0530

    new

--- arraysss.py
+++ arraysss.py
@@ -1,3 +1,14 @@
-a = 5 
+"""a = 5 
 print(a)
 print("Hello, World!")
+
+
+ recurssion """
+
+def list_sum(num_List):
+    if len(num_List) == 1:
+        return num_List[0]
+    else:
+        return num_List[0]+ list_sum(num_List[1:])
+    
+print(list_sum([2,3,4,5]))
