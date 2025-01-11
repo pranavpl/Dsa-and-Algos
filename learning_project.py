0b80847cb511288885a0a0898e225d6030e6d789
Pranav.PL <pranavpl.tvm@gmail.com>
2025-01-11 19:15:00 +0530

    python bassic1

---
+++ learning_project.py
@@ -0,0 +1,14 @@
+#data types & oprations
+def basic_operations():
+    #num and basic math 
+    x=5
+    y=5
+print(f"Basic math:")
+print(f"add: {x+y}")
+print(f"sub: {x-y}")
+print(f"mul:{x*y}")
+print(f"Division:{x/y}")
+print(f"Integer Division:{x//y}")
+print(f"Modulus:{x%y}")
+print(f"Power{x**y}")
+
