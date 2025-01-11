0714fd0a5a9d44729ad926653dfcd598ea671c61
Pranav.PL <pranavpl.tvm@gmail.com>
2025-01-11 19:17:00 +0530

    python bassic1

--- learning_project.py
+++ learning_project.py
@@ -3,12 +3,14 @@ def basic_operations():
     #num and basic math 
     x=5
     y=5
-print(f"Basic math:")
-print(f"add: {x+y}")
-print(f"sub: {x-y}")
-print(f"mul:{x*y}")
-print(f"Division:{x/y}")
-print(f"Integer Division:{x//y}")
-print(f"Modulus:{x%y}")
-print(f"Power{x**y}")
+    print(f"Basic math:")
+    print(f"add: { x + y }")
+    print(f"sub: {x-y}")
+    print(f"mul:{x*y}")
+    print(f"Division:{x/y}")
+    print(f"Integer Division:{x//y}")
+    print(f"Modulus:{x%y}")
+    print(f"Power{x**y}")
 
+
+basic_operations()
