0fc565fb86266210506e2b7b4dcfc61eebb01aee
Pranav.PL <pranavpl.tvm@gmail.com>
2025-01-20 20:29:37 +0530

    python bassic1

--- array ops.py
+++ array ops.py
@@ -1,9 +1,19 @@
-""" Print an Array """
+""" Print an Array 
 def printarray(arr):
     for element in arr:
         print(element,end="")
     print()
 
 arr = [1, 2, 3, 4, 5]
-printarray(arr)
+printarray(arr)"""
+
+"""Find Maximum and Minimum"""
+def findmaxmin(arr):
+    maxval = max(arr)
+    minval = min(arr)
+    return maxval,minval
+
+arr = [1,2,3,5]
+maxval,minval = findmaxmin(arr)
+print(maxval,minval)
 
