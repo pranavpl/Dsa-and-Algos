05ec028df6b4d7d3640af80c72939bdb033343d2
Pranav.PL <pranavpl.tvm@gmail.com>
2025-10-17 22:01:38 +0530

    newstart25

--- singlylinkedlist.py
+++ singlylinkedlist.py
@@ -1,12 +1,24 @@
-class node:
-    def __init__(self,data):
-        self.data =data
+class Node:
+    def __init__(self, data):
+        self.data = data
         self.next = None
-        pass 
-    def taraverseandprint(head):
-        currentnode = head
-        while currentnode:
-            print(currentnode.data,end="->")
-            currentnode = currentnode.next
-        print("null")
 
+def traverse_and_print(head):
+    current_node = head
+    while current_node:
+        print(current_node.data, end=" -> ")
+        current_node = current_node.next
+    print("null")
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+traverse_and_print(node1)
